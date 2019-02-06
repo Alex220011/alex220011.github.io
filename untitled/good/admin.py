d6f2adc737cdcfd583c080e93e31debaf4b00a3f
HappySouS <souspremium@gmail.com>
2019-02-07 02:58:01 +0500

    прв

--- untitled/good/admin.py
+++ untitled/good/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from good.models import *
 # Register your models here.
-admin.site.register(Store)
-#admin.site.register(products)
+#admin.site.register(Store)
+admin.site.register(products)
 
 
-@admin.register(products)
+@admin.register(Store)
 class VisitAdmin(admin.ModelAdmin):
-    list_display = ['name', 'store', 'price']
+    list_display = ['name']
--- untitled/good/models.py
+++ untitled/good/models.py
@@ -11,19 +11,20 @@ class NameModel(models.Model):
         return self.name
 
 
-class Store(NameModel):
-    description = models.TextField(verbose_name='Описание', default='')
-
-    class Meta:
-        verbose_name = 'Торговая точка'
-        verbose_name_plural = 'Торговые точки'
-
-
 class products(NameModel):
     description = models.TextField(verbose_name='Описание продукта', default='')
-    store = models.ForeignKey(verbose_name='Магазин', to=Store,null=True, on_delete=models.CASCADE)
     price = models.IntegerField(verbose_name='Цена', default=0)
 
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+
+class Store(NameModel):
+    description = models.TextField(verbose_name='Описание', default='')
+    #store = models.ForeignKey(verbose_name='Магазин', to=products, null=True, on_delete=models.CASCADE)
+    por = models.ManyToManyField(products)
+
+    class Meta:
+        verbose_name = 'Торговая точка'
+        verbose_name_plural = 'Торговые точки'
