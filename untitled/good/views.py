c623fd54d627450b45ad7765491bd446132a2501
HappySouS <souspremium@gmail.com>
2019-02-07 16:05:36 +0500

    прв

--- untitled/good/views.py
+++ untitled/good/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from good.models import Store
+from good.models import Store, products
 
 def home_page(request):
     return render(request, 'good/home-page.html')
@@ -9,10 +9,13 @@ def home(request):
     store_list = Store.objects.all()
     return render(request, 'good/home.html', dict(objects=store_list))
 
+
 #def product(request, id):
 #    products_list = get_object_or_404(product, id=id)
 #    return render(request, 'good/visit.html', dict(object=products_list))
 
+
 def start_visit(request, id):
     store = get_object_or_404(Store, id=id)
     return render(request, 'good/visit.html', dict(object=store))
+
